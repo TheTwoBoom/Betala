78dbb719df17d4ec9246e72a506645d01e8f633b
LeonxThexDeveloper <4131@htl.rennweg.at>
2026-05-15 00:03:46 +0200

    Basic UI für Home

--- app/src/main/java/app/myhtl/betala/MainActivity.kt
+++ app/src/main/java/app/myhtl/betala/MainActivity.kt
@@ -4,9 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
@@ -15,13 +24,16 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import app.myhtl.betala.ui.theme.BetalaTheme
-
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,7 +58,8 @@ fun BetalaApp() {
                     icon = {
                         Icon(
                             painterResource(it.icon),
-                            contentDescription = it.label
+                            contentDescription = it.label ,
+                            modifier = Modifier.size(40.dp)
                         )
                     },
                     label = { Text(it.label) },
@@ -57,10 +70,44 @@ fun BetalaApp() {
         }
     ) {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Column(
+                Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                ){
+
+            Header(modifier = Modifier.padding(innerPadding))
             Greeting(
-                name = "Android",
                 modifier = Modifier.padding(innerPadding)
+
             )
+
+
+            Row(Modifier.padding(innerPadding).fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(15.dp, Alignment.CenterHorizontally)
+            ) {
+                Button(
+
+                    onClick = {
+                        //Code für das Ausführen. Bitte nicht als anonyme Funktion!!!!!
+                    },
+                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiary)
+                ) {
+                    Text(text = "Continue")
+                }
+
+                Button(
+                    onClick = {
+
+                    },
+                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiary)
+
+                ) {
+                    Text(text = "Select level")
+                }
+
+                }
+            }
         }
     }
 }
@@ -69,16 +116,32 @@ enum class AppDestinations(
     val label: String,
     val icon: Int,
 ) {
-    HOME("Home", R.drawable.ic_home),
+
     FAVORITES("Favorites", R.drawable.ic_favorite),
-    PROFILE("Profile", R.drawable.ic_account_box),
+    HOME("Home", R.drawable.ic_home),
+    SETTINGS("Settings", R.drawable.outline_settings_24),
+
+}
+
+
+@Composable
+fun Header(modifier: Modifier = Modifier){
+    Text(
+        text = "Betala",
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.primary,
+        fontSize = 100.sp
+    )
+
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = "Welcome to Betala, Sudoko Variants!",
+        color = MaterialTheme.colorScheme.secondary,
+        fontSize = 20.sp,
+        modifier = modifier.padding(10.dp)
     )
 }
 
@@ -86,6 +149,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     BetalaTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
